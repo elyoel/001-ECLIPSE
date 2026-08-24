34b3d77460a78e2c225e72a20842f0034917bfa2
YOEL <YOEL@DESKTOP-YOEL>
2026-08-24 20:44:14 -0300

    Subiendo ejercicios resueltos

---
+++ balanzaElectronica/src/main/java/ar/edu/unlp/info/oo1/balanzaElectronica/Balanza.java
@@ -0,0 +1,47 @@
+package ar.edu.unlp.info.oo1.balanzaElectronica;
+
+public class Balanza {
+	private int cantidadDeProductos;
+	private double precioTotal;
+	private double pesoTotal;
+	
+	public Balanza () {
+		this.cantidadDeProductos = 0;
+		this.precioTotal = 0.0;
+		this.pesoTotal = 0.0;
+	}
+	
+	public int getCantidadDeProductos() {
+		return this.cantidadDeProductos;
+	}
+
+	public double getPrecioTotal() {
+		return this.precioTotal;
+	}
+
+	public double getPesoTotal() {
+		return this.pesoTotal;
+	}
+
+	public void ponerEnCero() {
+		this.pesoTotal = 0.0;
+		this.precioTotal = 0.0;
+		this.cantidadDeProductos = 0;
+	}
+	
+	public void agregarProducto (Producto producto) {
+		this.cantidadDeProductos++;
+		this.precioTotal += producto.getPrecio();
+		this.pesoTotal += producto.getPeso();
+	}
+	
+	public Ticket emitirTicket () {
+		Ticket ticket = new Ticket(
+				this.cantidadDeProductos,
+				this.pesoTotal,
+				this.precioTotal);
+		
+		return ticket;
+	}
+}
+
